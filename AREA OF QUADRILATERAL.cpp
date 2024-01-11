ec83b4d16dfab3265d78a91a37a39ac6e47bc98e
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2024-01-11 18:47:27 +0530

    Add files via upload
    Area of quadrilateral

---
+++ AREA OF QUADRILATERAL.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float d,h1,h2;
+	//TO FIND THE AREA OF QUARDILATERAL
+	cout<<"ENTER THE VALUE FOR D:";
+	cin>>d;
+	cout<<"ENTER THE VALUE FOR H1:";
+	cin>>h1;
+	cout<<"ENTER THE VALUE FOR H2:";
+	cin>>h2;
+	cout<<"THE AREA OF QUADRILATERAL:"<<0.5*d*(h1+h2);
+	return 0;
+}
